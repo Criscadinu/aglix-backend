8f857057ac4cf0ea83a2b51d065ca53cae94a041
Criscadinu <cris.g.cadinu@gmail.com>
2024-09-16 20:39:02 +0200

    New migration

---
+++ AglixBackend.Infrastructure/Migrations/20240916181423_AddImageUrlToAgile.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace AglixBackend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddImageUrlToAgile : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Agile",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Agile");
+        }
+    }
+}
